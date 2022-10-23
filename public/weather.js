5129bd4c43e48eb91c6abaa72b432534dca4c50e
Flew4k <michaeldamdinsuren@Michaels-MacBook-Pro.local>
2022-10-23 16:51:28 +0800

    added flew

--- public/weather.js
+++ public/weather.js
@@ -4,17 +4,26 @@ input.addEventListener("keypress", (event) => {
   if (event.keyCode == 13) {
     if (document.getElementById("input").value == "esugen") {
       document.getElementById("city").innerHTML = "I like Esugen";
-      document.getElementById("temperature").innerHTML = "❤️❤️❤️";
       document.getElementById("input").style.borderColor = "black";
       document.getElementById("status").innerHTML = "❤️❤️❤️";
-      let today = new Date().toISOString().slice(0, 10);
-      document.getElementById("date").innerHTML = today;
+      document.getElementById("date").innerHTML = "You are so beautiful esugen<3";
       document.getElementById("middle").style.background = "rgb(237 237 237)";
       document.getElementById("pic").src = "png/" + "heart" + ".png";
       document.getElementById("city").style.color = "black";
       document.getElementById("temperature").innerHTML = "❤️"
     } else {
-      getWeather(input.value);
+      if (document.getElementById("input").value == "flew") {
+        document.getElementById("city").innerHTML = "I am Flew4k";
+        document.getElementById("temperature").innerHTML = "213";
+        document.getElementById("input").style.borderColor = "black";
+        document.getElementById("status").innerHTML = "Flew4k";
+        document.getElementById("date").innerHTML = "4k means 4 times better than the normal Flew(Dulguun bagsh)";
+        document.getElementById("middle").style.background = "rgb(237 237 237)";
+        document.getElementById("pic").src = "png/" + "heart" + ".png";
+        document.getElementById("city").style.color = "black";
+      } else {
+        getWeather(input.value);
+      }
     }
   }
 });
