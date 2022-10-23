081affb3de8eb1d3b774bdf6dbe5e2b32c8bd4cb
Flew4k <michaeldamdinsuren@Michaels-MacBook-Pro.local>
2022-10-23 16:07:23 +0800

    added esugen

--- public/weather.js
+++ public/weather.js
@@ -2,7 +2,20 @@ let unixNow = Math.floor(Date.now() / 1000);
 const input = document.querySelector("#input");
 input.addEventListener("keypress", (event) => {
   if (event.keyCode == 13) {
-    getWeather(input.value);
+    if (document.getElementById("input").value == "esugen") {
+      document.getElementById("city").innerHTML = "I like Esugen";
+      document.getElementById("temperature").innerHTML = "❤️❤️❤️";
+      document.getElementById("input").style.borderColor = "black";
+      document.getElementById("status").innerHTML = "❤️❤️❤️";
+      let today = new Date().toISOString().slice(0, 10);
+      document.getElementById("date").innerHTML = today;
+      document.getElementById("middle").style.background = "rgb(237 237 237)";
+      document.getElementById("pic").src = "png/" + "heart" + ".png";
+      document.getElementById("city").style.color = "black";
+      document.getElementById("temperature").innerHTML = "❤️"
+    } else {
+      getWeather(input.value);
+    }
   }
 });
 async function getWeather(city) {
@@ -24,33 +37,21 @@ async function getWeather(city) {
     });
 }
 function displayData(cityName, tempK, status, sunrise, sunset) {
-  if (cityName == "esugen" && "Esugen") {
-    document.getElementById("city").innerHTML = "I like Esugen";
-    const tempC = "❤️❤️❤️";
-    document.getElementById("temperature").innerHTML = "❤️❤️❤️";
-    document.getElementById("input").style.borderColor = "black";
-    document.getElementById("status").innerHTML = "❤️❤️❤️";
-    let today = new Date().toISOString().slice(0, 10);
-    document.getElementById("date").innerHTML = today;
+  document.getElementById("city").innerHTML = cityName;
+  const tempC = tempK - 273;
+  document.getElementById("temperature").innerHTML = Math.floor(tempC) + "°";
+  document.getElementById("input").style.borderColor = "black";
+  document.getElementById("status").innerHTML = status;
+  let today = new Date().toISOString().slice(0, 10);
+  document.getElementById("date").innerHTML = today;
+  if (unixNow >= sunrise && unixNow <= sunset) {
     document.getElementById("middle").style.background = "rgb(237 237 237)";
-    document.getElementById("pic").src = "png/" + heart + ".png";
+    document.getElementById("pic").src = "png/" + status + ".png";
     document.getElementById("city").style.color = "black";
   } else {
-    document.getElementById("city").innerHTML = cityName;
-    const tempC = tempK - 273;
-    document.getElementById("temperature").innerHTML = Math.floor(tempC) + "°";
-    document.getElementById("input").style.borderColor = "black";
-    document.getElementById("status").innerHTML = status;
-    let today = new Date().toISOString().slice(0, 10);
-    document.getElementById("date").innerHTML = today;
-    if (unixNow >= sunrise && unixNow <= sunset) {
-      document.getElementById("middle").style.background = "rgb(237 237 237)";
-      document.getElementById("pic").src = "png/" + status + ".png";
-      document.getElementById("city").style.color = "black";
-    } else {
-      document.getElementById("middle").style.background = "#111827";
-      document.getElementById("pic").src = "png/" + status + "n.png";
-      document.getElementById("city").style.color = "white";
-    }
+    document.getElementById("middle").style.background = "#111827";
+    document.getElementById("pic").src = "png/" + status + "n.png";
+    document.getElementById("city").style.color = "white";
   }
 }
+
